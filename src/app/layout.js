b894710d81d74cbdf852f9bf7e454975a8a36481
usgnusmig <usgnusmig@gmail.com>
2023-12-27 23:09:32 +0100

    add header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,18 @@
+import "@/assets/style.scss";
+
 export const metadata = {
   title: "포트폴리오 사이트 만들기 🥺 Next-Site",
-  description:
-    "한가지 사이트를 다양한 프레임워크를 통해 만들어 보는 포트폴리오 사이트입니다.",
+  description: "Sungsu's Portfolio",
   keywords: [
     "포트폴리오",
-    "리액트",
-    "뷰",
-    "넥스트",
-    "웹스토리보이",
-    "portfolio",
-    "react.js",
-    "vue.js",
-    "next.js",
-    "webstoryboy",
+    "Portfolio",
+    "Woodwerker",
+    "Holzwerker",
+    "Holzarbeit",
+    "Schreiner",
+    "Tischler",
+    "usgnus",
+    "Sungsu",
   ],
 };
 
